8d8ec912cb9ee0bf42c0c4db5d3de6e2a17e6a9d
Ross Nesbit <Ross Nesbit@DESKTOP>
2024-06-09 12:20:46 -0500

    Update .net version from 6 to 8.

--- src/CsharpPowerViewTest/UnitTest1.cs
+++ src/CsharpPowerViewTest/UnitTest1.cs
@@ -10,8 +10,9 @@ namespace CsharpPowerViewTest
         [TestMethod]
         public async Task TestMethod1()
         {
-            IPowerViewAPIClient client = new PowerViewAPIClient(IPAddress.Parse("192.168.255.37"));
+            IPowerViewAPIClient client = new PowerViewAPIClient(IPAddress.Parse("192.168.255.110"));
 
+            var shades = await client.GetAllShadesAsync();
             //var result = await client.ActivatePowerViewSceneAsync(46143);
 
             var result = await client.SetShadePositionAsync(1, ShadePosition.OpenTop);
